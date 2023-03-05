ee3b5ebde01a53b327b1ef6f4643395be55969a8
PhillipWitkin <pwitkin273@gmail.com>
2023-03-05 11:27:32 -0500

    added logic in seeds.js to only insert when the table is empty

--- seeds.js
+++ seeds.js
@@ -2,18 +2,25 @@ const { Pool } = require('pg');
 const pool = require('./dbConn');
 
 // run seed SQL
-pool.query(`INSERT INTO pets (name, kind, age) VALUES 
-    ('Fido', 'Labradoodle', 5),
-    ('Skippy', 'Greyhound', 7),
-    ('Rollo', 'Golden Retriever', 5)`, 
-    (err, data) => {
-        if (err){
-            console.log("Insert failed");
-        } else {
-            console.log(data);
-        }
+pool.query(`SELECT COUNT(*) FROM pets`, (err, data) => {
+    console.log("number of existing rows: ", data.rows[0]['count']);
+    // only INSERT new rows if the table is currently empty
+    if (data.rows[0]['count'] == 0){
+        pool.query(`INSERT INTO pets (name, kind, age) VALUES 
+        ('Fido', 'Labradoodle', 5),
+        ('Skippy', 'Greyhound', 7),
+        ('Rollo', 'Golden Retriever', 5)`, 
+        (err, data) => {
+            if (err){
+                console.log("Insert failed");
+            } else {
+                console.log(data.rows[0]['count']);
+            }
+        });
     }
-);
+})
+
+
 
 // close connection
 pool.end();
