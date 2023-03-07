4e8430af47e65b70d93f7c0b37054d898934cfcc
Shuyi Hoo <anloveshu@Shuyis-MacBook-Air.local>
2023-03-06 17:20:27 -0800

    added get single item

--- expressServer.js
+++ expressServer.js
@@ -28,6 +28,30 @@ app.get('/ships', (req, res, next) => {
     });
 });
 
+// GET request to /pets/:id - Read one pet
+app.get('/ship/:id', (req, res, next) => {
+  // Get a single ship from the table
+  let id = Number.parseInt(req.params.id);
+  if (!Number.isInteger(id)){
+    res.status(404).send("No ship found with that ID");
+  }
+  console.log("spaceship ID: ", id);
+  
+  pool.query('SELECT * FROM spaceship WHERE id = $1', [id], (err, result) => {
+    if (err){
+      return next(err);
+    }
+    
+    let spaceship = result.rows[0];
+    console.log("Single Spaceship ID", id, "values:", spaceship);
+    if (spaceship){
+      return res.send(spaceship);
+    } else {
+      return res.status(404).send("No ship found with that ID");
+    }
+  });
+});
+
 
 app.listen(port, () => {
     // eslint-disable-next-line no-console
