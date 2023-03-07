1cffd7cadc1a0dfb868a37597d28a64588a2efa0
Shuyi Hoo <anloveshu@Shuyis-MacBook-Air.local>
2023-03-07 07:26:10 -0800

    added post function

--- expressServer.js
+++ expressServer.js
@@ -14,7 +14,7 @@ const dbConn = require('./dbConn');
 const pool = dbConn.getPool();
 
 
-  
+// GET request to all /ships - Read all data 
 app.get('/ships', (req, res, next) => {
     // Get allllll the rows in pets table
     pool.query('SELECT * FROM ships', (err, result) => {
@@ -28,7 +28,7 @@ app.get('/ships', (req, res, next) => {
     });
 });
 
-// GET request to /pets/:id - Read one pet
+// GET request to /ships/:id - Read one data
 app.get('/ships/:id', (req, res, next) => {
   // Get a single ship from the table
   let id = Number.parseInt(req.params.id);
@@ -50,6 +50,29 @@ app.get('/ships/:id', (req, res, next) => {
       return res.status(404).send("No ship found with that ID");
     }
   });
+});
+
+//post new ships
+app.post('/ships', (req, res, next) => {
+  //const age = Number.parseInt(req.body.age);
+  const {name, kind, manufacturer} = req.body;
+  console.log("Request body name, kind, age", name, kind, manufacturer);
+  // check request data - if everything exists and id is a number
+  if (name && kind && manufacturer){
+    pool.query('INSERT INTO ships (name, kind, manufacturer) VALUES ($1, $2, $3) RETURNING *', [name, kind, manufacturer], (err, data) => {
+      const spaceship = data.rows[0];
+      console.log("Created Ship: ", spaceship);
+      if (spaceship){
+        return res.send(spaceship);
+      } else {
+        return next(err);
+      }
+    });
+
+  } else {
+    return res.status(400).send("Unable to create ship from request body");
+  }
+
 });
 
 
